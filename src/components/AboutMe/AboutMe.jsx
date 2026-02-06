f459cac03f4fd81513dfc0006b22ddba257cad20
MinhHan195 <hanminhnguyen1905@gmail.com>
2026-02-07 04:14:11 +0700

    updates responsive

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -13,33 +13,34 @@ const AboutMe = () => {
                 </div>
                 <div className={`col-lg-6 p-0 m-0 ${style.about_me_content}`}>
                     <h4 className={style.about_me_title}>
-                        Minh Han - Fullstack Developer
+                        Minh Han - <br className={style.enter} />
+                        Fullstack Developer
                     </h4>
                     <p>
-                        My name is Minh Hân. I graduated with a degree in
-                        Information Technology from Can Tho University and am
-                        currently seeking opportunities as an Intern/Fresher
-                        Full-stack Developer. I previously gained internship
-                        experience as a DevOps Intern at the IT Department of
-                        Tay Do College.
+                        &emsp;&emsp;My name is Minh Hân. I graduated with a
+                        degree in Information Technology from Can Tho University
+                        and am currently seeking opportunities as an
+                        Intern/Fresher Full-stack Developer. I previously gained
+                        internship experience as a DevOps Intern at the IT
+                        Department of Tay Do College.
                     </p>
                     <p>
-                        I am passionate about programming and software
-                        development, particularly in web development and AI. I
-                        have experience working with technologies such as
-                        React.js, Vue.js, Node.js, and Express, as well as
+                        &emsp;&emsp;I am passionate about programming and
+                        software development, particularly in web development
+                        and AI. I have experience working with technologies such
+                        as React.js, Vue.js, Node.js, and Express, as well as
                         databases including SQL Server and MongoDB. I am always
                         eager to learn and stay up to date with new technologies
                         to continuously improve my skills.
                     </p>
                     <p>
-                        In addition to my academic studies, I actively
-                        participate in club and volunteer activities to develop
-                        my soft skills, while also learning from and sharing
-                        knowledge within the programming community. I believe
-                        that collaboration and knowledge sharing are essential
-                        for personal growth and meaningful contributions to the
-                        community.
+                        &emsp;&emsp;In addition to my academic studies, I
+                        actively participate in club and volunteer activities to
+                        develop my soft skills, while also learning from and
+                        sharing knowledge within the programming community. I
+                        believe that collaboration and knowledge sharing are
+                        essential for personal growth and meaningful
+                        contributions to the community.
                     </p>
                 </div>
             </div>
